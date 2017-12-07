64d76003c941e47df0d192b49ed0e2d149661442
sunyongjian <704928775@qq.com>
2017-12-08 00:25:57 +0800

    feat: simple static server
    update README.md
    
    feat: router

--- example/index.js
+++ example/index.js
@@ -1,15 +1,22 @@
 const Koa = require('../src/index');
 const fs = require('fs');
+const path = require('path');
 const app = new Koa();
 const bodyParse = require('../src/bodyParse');
 const cookieParse = require('../src/cookieParse');
+const static = require('../src/static');
+const Router = require('../src/koaRouter');
+
+const router = new Router();
+console.log(router, 'router');
 
 app.use(bodyParse());
 app.use(cookieParse());
+app.use(static(path.join(__dirname + '/static')));
 
+app.use(router.routes());
 // cookie
 app.use(async (ctx, next) => {
-  console.log(ctx.cookies);
   ctx.cookies().set('a', 'b', {
     httpOnly: true,
     path: '/',
@@ -18,20 +25,24 @@ app.use(async (ctx, next) => {
   ctx.cookies().set('c', 'd', {
     httpOnly: true,
   })
+  await next();
 })
 
+router.get('/', async(ctx) => {
+  ctx.body = 'Hello World'; 
+});
 // logger
 app.use(async (ctx, next) => {
   console.log(ctx.req.body, 'body');
-  next();
+  await next();
 })
 
 // body
 
 app.use(async (ctx, next) => {
   // TODO
-  ctx.body = 'hello world';
-  ctx.res.statusCode = 200;
+  // ctx.body = 'hello world';
+  // ctx.res.statusCode = 200;
   await next();
   console.log('wanle');
 });
